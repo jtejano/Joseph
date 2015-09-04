2139a5d219482e647123aa56eb10b41331503797
jtejano <papajowsef@gmail.com>
2015-09-04 16:40:29 +0800

    Windows.ps1
    TEST COMMIT 1

---
+++ Windows.ps1
@@ -0,0 +1,20 @@
+clear
+
+#Output file text
+$Test01_Text = "1,Verify Hostname,"
+$Test02_Text = "2,Verify COM+ services,"
+$Test03_Text = "3,Verify Firewall Configuration,"
+$Test04_Text = "4,Verify Advance Firewall Configuration,"
+$Test05_Text = "5,Verify Windows Licence Activation,"
+$Test06_Text = "6,Verify Local Security Policy configuration,"
+$Test07_Text = "7,Verify HDD Configuration,"
+$Test08_Text = "8,Verify Uptime Agent Configuration,"
+$Test09_Text = "9,Verify SEP agent installation,"
+$Test10_Text = "10,Verify BigFix agent installation,"
+$Test11_Text = "11,Verify Netbackup agent installation,"
+$Test12_Text = "12,Verify Production NIC configuration,"
+$Test13_Text = "13,Verify Backup NIC configuration,"
+$Test14_Text = "14,Verify Production Gateway connectivity,"
+$Test15_Text = "15,Verify Backup Master Server connectivity,"
+$Test16_Text = "16,Verify Backup Appliances connectivity,"
+$Test17_Text = "17,Verify VM is added to Hcloud Domain where applicable"
